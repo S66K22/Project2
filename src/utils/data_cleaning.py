4c7bb535634065af5054c8b7ed0e9b255c320151
saeed <kermoonshahi@outlook.com>
2026-09-13 22:51:49 +0330

    add check_images_format function to check files are really images or not

--- src/utils/data_cleaning.py
+++ src/utils/data_cleaning.py
@@ -5,3 +5,10 @@ from pathlib import Path
 def find_file_format(filepath):
     file_type = magic.from_file(filepath, mime=True)
     return file_type
+
+def check_images_format(dir):
+    directory = Path(dir)
+
+    for path in directory.iterdir():
+        if find_file_format(path) != "image/jpeg":
+            print(path)
