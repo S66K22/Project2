3fb23b96c657262a27890100a81b2887b86b23c3
saeed <kermoonshahi@outlook.com>
2026-09-13 23:02:46 +0330

    add logger to data_cleaning

--- src/utils/data_cleaning.py
+++ src/utils/data_cleaning.py
@@ -1,14 +1,19 @@
-import magic
+import logging
 from pathlib import Path
+
+import magic
+
+logger = logging.getLogger(__name__)
 
 
 def find_file_format(filepath):
     file_type = magic.from_file(filepath, mime=True)
     return file_type
 
+
 def check_images_format(dir):
     directory = Path(dir)
 
     for path in directory.iterdir():
         if find_file_format(path) != "image/jpeg":
-            print(path)
+            logger.info(f"File with path {path} is not an image.")
