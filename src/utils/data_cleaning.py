64dedfedaf418535708ff8a6583e9506ab12f027
saeed <kermoonshahi@outlook.com>
2026-09-14 21:03:39 +0330

    Add image_preprocessing. It iterates through dirs and checks whether images are valid or not. I also change aspect ratios.

--- src/utils/data_cleaning.py
+++ src/utils/data_cleaning.py
@@ -42,28 +42,41 @@ def is_image_size_valid(height, width):
     if height < 32 or width < 32:
         ret = False
     ratio = width / height
-    if ratio < 0.5 or ratio > 2.0:
+    if ratio < 0.33 or ratio > 3.:
         ret = False
     return ret
 
 
-def separate_valid_invalid_images(directory, corrupted_files_dir):
+def separate_valid_invalid_images(path, corrupted_files_dir):
 
-    for path in directory.iterdir():
-        if find_file_format(path) != "image/jpeg":
-            logger.info(f"File with path {path} is not an image.")
-            corrupted_files_dir = corrupted_files_dir / path.parent.name
-            move_file(path, corrupted_files_dir)
-            continue
+    if find_file_format(path) != "image/jpeg":
+        logger.info(f"File with path {path} is not an image.")
+        corrupted_files_dir = corrupted_files_dir / path.parent.name
+        move_file(path, corrupted_files_dir)
 
+    else:
         is_valid, width, height = can_image_be_loaded(path)
-        if not is_valid:
-            logger.info(f"Invalid/corrupted image: {path}")
-            corrupted_files_dir = corrupted_files_dir / path.parent.name
-            move_file(path, corrupted_files_dir)
-            continue
-        if is_image_size_valid(height, width):
+        if not is_valid or not is_image_size_valid(height, width):
             logger.info(f"Invalid/corrupted image: {path}")
             corrupted_files_dir = corrupted_files_dir / path.parent.name
             move_file(path, corrupted_files_dir)
 
+def get_file_paths(directory):
+    return [
+        path
+        for path in directory.rglob("*")
+        if path.is_file()
+    ]
+
+def image_preprocessing():
+    data_dir = Path("data")
+    train_dir = data_dir / "train"
+    test_dir = data_dir / "test"
+    unclean_dir = data_dir / "unclean"
+    corrupted_files_dir = data_dir / "corrupted"
+
+    for dir in [train_dir, test_dir, unclean_dir]:
+        for path in get_file_paths(dir):
+            separate_valid_invalid_images(path, corrupted_files_dir)
+
+image_preprocessing()
