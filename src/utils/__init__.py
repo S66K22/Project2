d17a4675e1ec0b5ad6583ec8e0e220db229c047a
saeed <kermoonshahi@outlook.com>
2026-09-15 20:30:16 +0330

    format with black

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,6 +1,12 @@
 from .data_cleaning import image_preprocessing
+from .display_images import ims_show
 from .logging_config import setup_logging
 from .model import create_model, log_number_of_params
-from .display_images import ims_show
 
-__all__ = ["setup_logging", "image_preprocessing", "create_model", "ims_show", "log_number_of_params"]
+__all__ = [
+    "setup_logging",
+    "image_preprocessing",
+    "create_model",
+    "ims_show",
+    "log_number_of_params",
+]
